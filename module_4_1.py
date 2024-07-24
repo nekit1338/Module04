ac089054d70614a527064ae80122f47b9ed7b0e4
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-07-25 01:33:24 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as dv
+from true_math import divide as dv2
+
+result1 = dv(69, 3)
+result2 = dv(3, 0)
+result3 = dv2(49, 7)
+result4 = dv2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
